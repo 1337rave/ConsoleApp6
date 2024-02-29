b85b5c3b2532024a27eeb51df0c82d11014f62cf
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-29 19:28:33 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Matrix
+{
+    private int[,] matrix;
+    private int rows;
+    private int columns;
+
+    public Matrix(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        matrix = new int[rows, columns];
+    }
+
+    public void InputMatrix()
+    {
+        Console.WriteLine("Enter matrix elements:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Enter element [{i},{j}]: ");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+
+    public void DisplayMatrix()
+    {
+        Console.WriteLine("Matrix:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public int FindMax()
+    {
+        int max = matrix[0, 0];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (matrix[i, j] > max)
+                {
+                    max = matrix[i, j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public int FindMin()
+    {
+        int min = matrix[0, 0];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (matrix[i, j] < min)
+                {
+                    min = matrix[i, j];
+                }
+            }
+        }
+        return min;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of rows in the matrix:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of columns in the matrix:");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        Matrix matrix = new Matrix(rows, columns);
+        matrix.InputMatrix();
+        matrix.DisplayMatrix();
+
+        int max = matrix.FindMax();
+        int min = matrix.FindMin();
+
+        Console.WriteLine($"Maximum element in the matrix: {max}");
+        Console.WriteLine($"Minimum element in the matrix: {min}");
+    }
+}
