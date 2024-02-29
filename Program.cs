3981c0a9ff42cc2f1ba6ad661db1d740788636d1
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-29 19:22:23 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class RangeMultiplier
+{
+    public int Start { get; }
+    public int End { get; }
+
+    public RangeMultiplier(int start, int end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public int GetProduct()
+    {
+        int product = 1;
+        for (int i = Start; i <= End; i++)
+        {
+            product *= i;
+        }
+        return product;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int startRange = 1;
+        int endRange = 5;
+
+        RangeMultiplier rangeMultiplier = new RangeMultiplier(startRange, endRange);
+        int product = rangeMultiplier.GetProduct();
+
+        Console.WriteLine($"Добуток чисел у діапазоні від {startRange} до {endRange} дорівнює {product}");
+    }
+}
